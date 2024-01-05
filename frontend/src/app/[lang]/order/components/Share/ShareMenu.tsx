13bb5cfe677011e9ecad86fc392b3947451300b9
Benny (Kim Wooseok) <VVSOGI@gmail.com>
2024-01-06 00:55:28 +0900

    Update ShareMenu component change name create -> print

--- frontend/src/app/[lang]/order/components/Share/ShareMenu.tsx
+++ frontend/src/app/[lang]/order/components/Share/ShareMenu.tsx
@@ -13,7 +13,7 @@ export default function ShareMenu() {
     return (
         language && (
             <div
-                onClick={() => router.push(`/${language}/order/create`)}
+                onClick={() => router.push(`/${language}/order/print`)}
                 className="active:bg-gray-700 hover:bg-gray-400 hover:last:text-white hover:border-gray-400 dark:active:bg-white dark:hover:border-white dark:border-[transperant] dark:hover:last:text-dark-bg-title dark:bg-bg-title last:text-gray-700 fixed right-16 bottom-16 w-[50px] h-[50px] flex items-center justify-center rounded-full border border-bg-title bg-white cursor-pointer shadow-share"
             >
                 <PencilSquareIcon className="w-[35px] h-[35px]" />
