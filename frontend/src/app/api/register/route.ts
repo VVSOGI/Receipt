2d69918112cf1c3a20633de85d04e5ce2d6fbc76
김우석 <benny@gim-useogs-MacBook-Air.local>
2024-04-03 02:08:07 +0900

    Add password validation and registration API call

--- frontend/src/app/api/register/route.ts
+++ frontend/src/app/api/register/route.ts
@@ -1,9 +1,18 @@
+import axios from 'axios'
 import { NextRequest, NextResponse } from 'next/server'
-
-export async function GET(req: NextRequest) {
-    return NextResponse.json('hello world')
-}
 
 export async function POST(req: NextRequest) {
     const { email, password, checkPassword } = (await req.json()) as { email: string; password: string; checkPassword: string }
+    try {
+        if (password !== checkPassword) return NextResponse.json({ message: 'Passwords do not match' }, { status: 400 })
+
+        const { data } = await axios.post(`${process.env.NODE_BACKEND_URL}/users/register`, {
+            email,
+            password
+        })
+
+        return NextResponse.json(data)
+    } catch (err: any) {
+        return NextResponse.json({ message: err.response.data.message }, { status: err.response.data.statusCode })
+    }
 }
